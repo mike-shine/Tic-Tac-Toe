7332f59076b874b120dc20fe5761125398f7a1ef
Mike Shine <michaelbshine@outlook.com>
2021-02-09 11:00:30 -0800

    added player mark logic inside player factory function

--- app.js
+++ app.js
@@ -12,21 +12,70 @@ const gameBoard = (function() {
 })();
 
 const displayController = (function() {
-  let topLeft = document.querySelector('.rowOne .spaceOne').textContent = gameBoard.gameArray[0][0];
+  const topLeft = document.querySelector('.rowOne .spaceOne');
+  topLeft.textContent = gameBoard.gameArray[0][0];
 
-  let topCenter = document.querySelector('.rowOne .spaceTwo').textContent = gameBoard.gameArray[0][1];
+  const topCenter = document.querySelector('.rowOne .spaceTwo');
+  topCenter.textContent = gameBoard.gameArray[0][1];
 
-  let topRight = document.querySelector('.rowOne .spaceThree').textContent = gameBoard.gameArray[0][2];
+  const topRight = document.querySelector('.rowOne .spaceThree');
+  topRight.textContent = gameBoard.gameArray[0][2];
 
-  let middleLeft = document.querySelector('.rowTwo .spaceOne').textContent = gameBoard.gameArray[1][0];
+  const middleLeft = document.querySelector('.rowTwo .spaceOne');
+  middleLeft.textContent = gameBoard.gameArray[1][0];
 
-  let center = document.querySelector('.rowTwo .spaceTwo').textContent = gameBoard.gameArray[1][1];
+  const center = document.querySelector('.rowTwo .spaceTwo');
+  center.textContent = gameBoard.gameArray[1][1];
 
-  let middleRight = document.querySelector('.rowTwo .spaceThree').textContent = gameBoard.gameArray[1][2];
+  const middleRight = document.querySelector('.rowTwo .spaceThree');
+  middleRight.textContent = gameBoard.gameArray[1][2];
 
-  let bottomLeft = document.querySelector('.rowThree .spaceOne').textContent = gameBoard.gameArray[2][0];
+  const bottomLeft = document.querySelector('.rowThree .spaceOne');
+  bottomLeft.textContent = gameBoard.gameArray[2][0];
 
-  let bottomCenter = document.querySelector('.rowThree .spaceTwo').textContent = gameBoard.gameArray[2][1];
+  const bottomCenter = document.querySelector('.rowThree .spaceTwo');
+  bottomCenter.textContent = gameBoard.gameArray[2][1];
 
-  let bottomRight = document.querySelector('.rowThree .spaceThree').textContent = gameBoard.gameArray[2][2];
+  const bottomRight = document.querySelector('.rowThree .spaceThree');
+  bottomRight.textContent = gameBoard.gameArray[2][2];
+
+  return {topLeft, topCenter, topRight, middleLeft, center, middleRight, bottomLeft, bottomCenter, bottomRight};
 })();
+
+const Player = function(name) {
+  const movesPlayed = [];
+  let amIPlayer1;
+
+  if (player1) {
+    amIPlayer1 = true;
+    player1 = false;
+  } else {
+    amIPlayer1 = false;
+  }
+
+  if (amIPlayer1) {
+    const mark = prompt(`${name}, are you Xs or Os? Please enter either a capital X or capital O.`);
+    if (mark === 'X') {
+      xHasBeenTaken = true;
+    }
+  } else {
+    if (xHasBeenTaken) {
+      const mark = 'O';
+    } else {
+      const mark = 'X';
+    }
+  }
+};
+
+const addMarksToBoard = (function() {
+
+})();
+
+let player1 = true;
+let xHasBeenTaken = false;
+let currentPlayer;
+let currentTurn = 0;
+
+const mike = Player('Mike');
+const mandy = Player('Mandy');
+
