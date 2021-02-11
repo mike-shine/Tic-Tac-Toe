69bee6476919c5cd76256b7c3cf98f24204a0902
Mike Shine <michaelbshine@outlook.com>
2021-02-11 10:06:33 -0800

    win logic works for both players and all win possibilities

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ clearButton.addEventListener('click', () => {
     displayController[space].textContent = '';
     gameBoard.gameArray[index] = '';
   });
+  allPlayers.forEach(player => {
+    player.currentMarks = [];
+  })
   currentPlayer = allPlayers[0];
   turnsElapsed = 0;
 });
@@ -56,6 +59,7 @@ const Player = function(name) {
   const movesPlayed = [];
   let amIPlayer1;
   let mark;
+  let currentMarks = [];
 
   if (player1) {
     amIPlayer1 = true;
@@ -76,7 +80,7 @@ const Player = function(name) {
       mark = 'X';
     }
   }
-  return {playerName, movesPlayed, amIPlayer1, mark};
+  return {playerName, movesPlayed, amIPlayer1, mark, currentMarks};
 };
 
 let player1 = true;
@@ -90,30 +94,17 @@ allPlayers.push(mandy);
 let currentPlayer = allPlayers[0];
 
 
-
-let turnsElapsed = 0;
-
-const addMarksToBoard = (function() {
-  let gameBoardTable = document.querySelector('.gameBoardTable');
-  gameBoardTable.addEventListener('click', () => {
-     if (allPlayers.indexOf(currentPlayer) === 0) {
-       currentPlayer = allPlayers[1];
-     } else {
-       currentPlayer = allPlayers[0];
-     }
-  });
-
-  Object.keys(displayController).forEach((space, index) => {
-    displayController[space].addEventListener('click', () => {
-      if (displayController[space].textContent === '') {
-        displayController[space].textContent = currentPlayer.mark;
-        gameBoard.gameArray[index] = currentPlayer.mark;
-        turnsElapsed++;
-      }
-    })
-  });
-})();
-
+const winningCombos = (function() {
+  return [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7]
+]})();
 
 const gameOver = (function() {
 
@@ -131,12 +122,23 @@ const gameOver = (function() {
     }, 100);
   };
 
+  const threeInARow = function() {
+    return winningCombos.some(combo => {
+      return currentPlayer.currentMarks.includes(combo[0])
+      && currentPlayer.currentMarks.includes(combo[1])
+      && currentPlayer.currentMarks.includes(combo[2])
+    });
+  }
+
   let gameBoardTable = document.querySelector('.gameBoardTable');
   gameBoardTable.addEventListener('click', () => {
-     if (turnsElapsed === 9) {
-       tieGame();
-     }
+     if (threeInARow()) {
+      playerWins();
+     } else if (turnsElapsed === 9) {
+      tieGame();
+    }
   });
+  return {threeInARow};
 })();
 
 
@@ -145,6 +147,31 @@ const gameOver = (function() {
 
 
 
+let turnsElapsed = 0;
+
+const addMarksToBoard = (function() {
+  Object.keys(displayController).forEach((space, index) => {
+    displayController[space].addEventListener('click', () => {
+      if (displayController[space].textContent === '') {
+        displayController[space].textContent = currentPlayer.mark;
+        gameBoard.gameArray[index] = currentPlayer.mark;
+        currentPlayer.currentMarks.push(index + 1);
+        turnsElapsed++;
+      }
+    })
+  });
+
+  let gameBoardTable = document.querySelector('.gameBoardTable');
+  gameBoardTable.addEventListener('click', () => {
+     if (allPlayers.indexOf(currentPlayer) === 0) {
+       currentPlayer = allPlayers[1];
+     } else {
+       currentPlayer = allPlayers[0];
+     }
+  });
+
+
+})();
 
 
 
